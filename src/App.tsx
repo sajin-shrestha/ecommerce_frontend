47f94cf5a99ca9f8c74170351badb90da4d2affc
nomadDEV <shresthasajin59@gmail.com>
2024-11-22 21:28:03 +0545

    base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Typography } from 'antd'
+import { Content } from 'antd/es/layout/layout'
+
+const { Title } = Typography
+
 const App = () => {
-  return <div>Hello world!</div>
+  return (
+    <Content>
+      <Title>Hello World</Title>
+    </Content>
+  )
 }
 
 export default App
